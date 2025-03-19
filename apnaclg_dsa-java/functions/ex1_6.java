75b84b82825b6ecbe270a98d61fb75edb046f3ac
S-dweep <mondalsayandeep2004@gmail.com>
2025-03-19 18:24:23 +0530

    Added untracked files and resolved issues

--- apnaclg_dsa-java/functions/ex1_6.java
+++
@@ -1,7 +0,0 @@
-class ex1_6{
-    public static void main(String[] args) {
-        do { 
-            
-        } while (true);
-    }
-}
--- apnaclg_dsa-java/functions/ex1_8.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class ex1_8{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        int n=sc.nextInt();
-        System.out.println((int)Math.pow(x,n));
-    }
-}
